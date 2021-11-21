e9ef749c7350e8399ba44d759c1e7628a6adfb3b
WangYX <1792154221@qq.com>
2021-11-21 17:42:11 +0800

    fix:element-ui出现展开后子菜单宽度多出1px的问题

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,20 +3,20 @@
     <!-- 头部区域 -->
     <el-header>
       <div class="home-logo-title">
-        <img src="../assets/logo.png"
+        <img src="../assets/wxmetro_logo.jpg"
              alt=""
              class="home-logo">
-        <span class="home-title">VUE</span>
+        <span class="home-title">无锡地铁4号线</span>
       </div>
-      <el-button type="info"
-                 @click="logout">退出登录</el-button>
+      <el-button @click="logout"
+                 round>退出登录</el-button>
     </el-header>
     <!-- 主体区域 -->
     <el-container>
       <!-- 侧边栏 -->
       <el-aside width="200px">
         <!-- 侧边菜单栏区域 -->
-        <el-menu background-color="#333744"
+        <el-menu background-color="#8B008b"
                  text-color="#fff"
                  active-text-color="#ffd04b">
           <!-- 一级菜单        -->
@@ -29,14 +29,24 @@
               <span>刘潭站</span>
             </template>
             <!-- 二级菜单 -->
-            <el-menu-item index="1-4-1">
-              <template slot="title">
-                <!-- 图标 -->
-                <i class="el-icon-menu"></i>
-                <!-- 文本 -->
-                <span>小系统</span>
-              </template>
-            </el-menu-item>
+            <el-menu-item index="1-1">小系统</el-menu-item>
+            <el-menu-item index="1-2">大系统</el-menu-item>
+            <el-menu-item index="1-3">空调水系统</el-menu-item>
+          </el-submenu>
+
+          <!-- 一级菜单        -->
+          <el-submenu index="2">
+            <!-- 一级菜单模板区域 -->
+            <template slot="title">
+              <!-- 图标 -->
+              <i class="el-icon-monitor"></i>
+              <!-- 文本 -->
+              <span>广石路站</span>
+            </template>
+            <!-- 二级菜单 -->
+            <el-menu-item index="2-1">小系统</el-menu-item>
+            <el-menu-item index="2-2">大系统</el-menu-item>
+            <el-menu-item index="2-3">空调水系统</el-menu-item>
           </el-submenu>
         </el-menu>
       </el-aside>
@@ -62,7 +72,7 @@ export default {
   height: 100%;
 }
 .el-header {
-  background-color: #373d41;
+  background-color: #800080;
   display: flex;
   justify-content: space-between;
   /* padding-left: 0; */
@@ -82,7 +92,10 @@ export default {
   margin-left: 15px;
 }
 .el-aside {
-  background-color: #333744;
+  background-color: #800080;
+}
+.el-menu {
+  border-right-width: 0;
 }
 .el-main {
   background-color: #eaedf1;
