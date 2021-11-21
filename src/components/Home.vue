deb2c03a07096195f8a535262b408735056eebeb
WangYX <1792154221@qq.com>
2021-11-21 16:28:24 +0800

    完成主页布局和侧边栏菜单区域

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,8 +1,49 @@
 <template>
-  <div>
-    <el-button type="info"
-               @click="logout">退出登录</el-button>
-  </div>
+  <el-container class="home-container">
+    <!-- 头部区域 -->
+    <el-header>
+      <div class="home-logo-title">
+        <img src="../assets/logo.png"
+             alt=""
+             class="home-logo">
+        <span class="home-title">VUE</span>
+      </div>
+      <el-button type="info"
+                 @click="logout">退出登录</el-button>
+    </el-header>
+    <!-- 主体区域 -->
+    <el-container>
+      <!-- 侧边栏 -->
+      <el-aside width="200px">
+        <!-- 侧边菜单栏区域 -->
+        <el-menu background-color="#333744"
+                 text-color="#fff"
+                 active-text-color="#ffd04b">
+          <!-- 一级菜单        -->
+          <el-submenu index="1">
+            <!-- 一级菜单模板区域 -->
+            <template slot="title">
+              <!-- 图标 -->
+              <i class="el-icon-monitor"></i>
+              <!-- 文本 -->
+              <span>刘潭站</span>
+            </template>
+            <!-- 二级菜单 -->
+            <el-menu-item index="1-4-1">
+              <template slot="title">
+                <!-- 图标 -->
+                <i class="el-icon-menu"></i>
+                <!-- 文本 -->
+                <span>小系统</span>
+              </template>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <!-- 右侧内容主体 -->
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
@@ -17,4 +58,33 @@ export default {
 </script>
 
 <style scoped>
+.home-container {
+  height: 100%;
+}
+.el-header {
+  background-color: #373d41;
+  display: flex;
+  justify-content: space-between;
+  /* padding-left: 0; */
+  align-items: center;
+  color: #fff;
+  font-size: 20px;
+}
+.home-logo-title {
+  display: flex;
+  align-items: center;
+}
+.home-logo {
+  height: 40px;
+  width: 40px;
+}
+.home-title {
+  margin-left: 15px;
+}
+.el-aside {
+  background-color: #333744;
+}
+.el-main {
+  background-color: #eaedf1;
+}
 </style>
